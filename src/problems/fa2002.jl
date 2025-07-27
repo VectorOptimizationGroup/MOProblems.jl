86467a2daf8033de8ffb6b1e5ddc829662f98342
Souza-DR <danilogyn01@gmail.com>
2025-07-27 03:37:08 -0300

    WIP: problems fa2002 and far2002 under construction

---
+++ src/problems/fa2002.jl
@@ -0,0 +1,4 @@
+"""
+
+A. Farhang-Mehr and S. Azarm, "Diversity assessment of Pareto optimal solution sets: an entropy approach," Proceedings of the 2002 Congress on Evolutionary Computation. CEC'02 (Cat. No.02TH8600), Honolulu, HI, USA, 2002, pp. 723-728 vol.1, DOI: 10.1109/CEC.2002.1007015.
+"""
---
+++ src/problems/far2002.jl
@@ -0,0 +1,3 @@
+"""
+M. Farina, "A neural network based generalized response surface multiobjective evolutionary algorithm," Proceedings of the 2002 Congress on Evolutionary Computation. CEC'02 (Cat. No.02TH8600), Honolulu, HI, USA, 2002, pp. 956-961 vol.1, DOI: 10.1109/CEC.2002.1007054.
+"""
