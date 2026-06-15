4adb2c072cccbdff46c87dd9d0549cfc09327259
Souza-DR <danilogyn01@gmail.com>
2026-06-14 21:19:56 -0300

    test(evaluation): cover in-place buffers

--- test/interface/numeric_type_contract.jl
+++ test/interface/numeric_type_contract.jl
@@ -22,6 +22,11 @@ function _test_objective_and_jacobian_types(prob, ::Type{T}) where {T <: Abstrac
     @test eltype(values) === T
     @test length(values) == prob.nobj
 
+    values_buffer = Vector{T}(undef, prob.nobj)
+    @test MOProblems.eval_f!(values_buffer, prob, x) === values_buffer
+    @test eltype(values_buffer) === T
+    @test length(values_buffer) == prob.nobj
+
     value = MOProblems.eval_f(prob, x, 1)
     @test typeof(value) === T
 
@@ -29,9 +34,19 @@ function _test_objective_and_jacobian_types(prob, ::Type{T}) where {T <: Abstrac
     @test eltype(J) === T
     @test size(J) == (prob.nobj, prob.nvar)
 
+    J_buffer = Matrix{T}(undef, prob.nobj, prob.nvar)
+    @test MOProblems.eval_jacobian!(J_buffer, prob, x) === J_buffer
+    @test eltype(J_buffer) === T
+    @test size(J_buffer) == (prob.nobj, prob.nvar)
+
     row = MOProblems.eval_jacobian_row(prob, x, 1)
     @test eltype(row) === T
     @test length(row) == prob.nvar
+
+    row_buffer = Vector{T}(undef, prob.nvar)
+    @test MOProblems.eval_jacobian_row!(row_buffer, prob, x, 1) === row_buffer
+    @test eltype(row_buffer) === T
+    @test length(row_buffer) == prob.nvar
 end
 
 function _test_hessian_types(prob, ::Type{T}) where {T <: AbstractFloat}
@@ -45,6 +60,17 @@ function _test_hessian_types(prob, ::Type{T}) where {T <: AbstractFloat}
     H = MOProblems.eval_hessian_row(prob, x, 1)
     @test eltype(H) === T
     @test size(H) == (prob.nvar, prob.nvar)
+
+    Hs_buffer = [Matrix{T}(undef, prob.nvar, prob.nvar) for _ in 1:prob.nobj]
+    @test MOProblems.eval_hessian!(Hs_buffer, prob, x) === Hs_buffer
+    @test length(Hs_buffer) == prob.nobj
+    @test all(H -> eltype(H) === T, Hs_buffer)
+    @test all(H -> size(H) == (prob.nvar, prob.nvar), Hs_buffer)
+
+    H_buffer = Matrix{T}(undef, prob.nvar, prob.nvar)
+    @test MOProblems.eval_hessian_row!(H_buffer, prob, x, 1) === H_buffer
+    @test eltype(H_buffer) === T
+    @test size(H_buffer) == (prob.nvar, prob.nvar)
 end
 
 @testset "Numeric Type Contract" begin
