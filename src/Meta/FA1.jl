90035ec6c2d2e4a9d8be2f1ef50e5ba3fe1be529
Souza-DR <danilogyn01@gmail.com>
2026-07-18 10:42:15 -0300

    .

--- src/Meta/FA1.jl
+++ src/Meta/FA1.jl
@@ -1,7 +1,7 @@
 FA1_meta = ProblemMeta(
     dimension = FixedDimension(3, 3),
-    name = "FA1",               # Official problem name
-    has_bounds = true,           # Variables have box constraints?
-    has_jacobian = true,         # Analytical jacobian provided?
+    name = "FA1",
+    has_bounds = true,
+    has_jacobian = true,
     strict_convexity = [:not_strictly_convex, :not_strictly_convex, :not_strictly_convex],
 )
